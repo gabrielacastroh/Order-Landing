b33569131b1d09383d6b95af29b57ad2031503f6
Gabriela Castro <38534250+gabrielach99@users.noreply.github.com>
2021-03-13 17:05:35 -0500

    primera parte

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- BOOTSTRAP -->
+	<link rel="stylesheet" href="/Parcial_1/styles/bootstrap.min.css">
+	<link rel="stylesheet" href="/Parcial_1/styles/style.css">
+	<title>Brasa Loca</title>
+</head>
+
+<body>
+	<div class="first">
+		<!-- NAVEGATION -->
+		<nav class="navbar navbar-expand-sm" style="background-color : #1e212d">
+			<div class="container-fluid">
+				<div class="collapse navbar-collapse">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a href="#" class="nav-link ">Inicio</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link ">Registrar</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link">Pedidos</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link ">Menú</a>
+						</li>
+					</ul>
+				</div>
+				<div class="collapse navbar-collapse justify-content-center">
+					<a href="index.php" class="navbar-brand">
+						<img src="/Parcial_1/img/logo.png" alt="" class="d-inline-block" width="250" height="30">
+					</a>
+				</div>
+				<div class="collapse navbar-collapse justify-content-end">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a href="#" class="nav-link">About Us</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link ">Galería</a>
+						</li>
+						<li class="nav-item">
+							<a href="#" class="nav-link ">Contact Us</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<div class="row align-items-stretch w-100 b">
+			<div class="col-2 ">
+				<div class="row py-3">
+					<div class="col text-center">
+						<p class="title-card">Nuestras Malteadas</p>
+					</div>
+				</div>
+				<div class="row py-2">
+					<div class="col-12">
+						<div class="card" style="width: 13rem;">
+							<img src="/Parcial_1/img/malteada-1.jpg" class="card-img-top" alt="malteda-1">
+						</div>
+					</div>
+				</div>
+				<div class="row py-2">
+					<div class="col-12">
+						<div class="card" style="width: 13rem;">
+							<img src="/Parcial_1/img/malteada-2.jpg" class="card-img-top" alt="malteda-2">
+						</div>
+					</div>
+				</div>
+				<div class="row py-2">
+					<div class="col-12">
+						<div class="card" style="width: 13rem;">
+							<img src="/Parcial_1/img/malteada-3.jpg" class="card-img-top" alt="malteda-3">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-8">
+				<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+					<div class="carousel-inner">
+						<div class="carousel-item active">
+							<img src="/Parcial_1/img/hamburguesa.jpg" class="d-block w-100" alt="imagen_1">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>Bienvenidos a Brasa Loca</h5>
+								<div class="slider-btn">
+									<a href="#titulo">
+										<div class="btn btn-1">
+											Hacer Pedido
+										</div>
+									</a>
+								</div>
+								<div class="i">
+									<i>"Un lugar inolvidable"</i>
+								</div>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<img src="/Parcial_1/img/pizza.jpg" class="d-block w-100" alt="...">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>Bienvenidos a Brasa Loca</h5>
+								<div class="slider-btn">
+									<a href="#titulo">
+										<div class="btn btn-1">
+											Hacer Pedido
+										</div>
+									</a>
+								</div>
+								<div class="i">
+									<i>"Un lugar inolvidable"</i>
+								</div>
+							</div>
+						</div>
+						<div class="carousel-item">
+							<img src="/Parcial_1/img/perro.jpg" class="d-block w-100" alt="...">
+							<div class="carousel-caption d-none d-md-block">
+								<h5>Bienvenidos a Brasa Loca</h5>
+								<div class="slider-btn">
+									<a href="#titulo">
+										<div class="btn btn-1">
+											Hacer Pedido
+										</div>
+									</a>
+								</div>
+								<div class="i">
+									<i>"Un lugar inolvidable"</i>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-2">
+				<div class="row py-3">
+					<div class="col text-center">
+						<p class="title-card">Nuestras Bebidas</p>
+					</div>
+				</div>
+				<div class="row py-2">
+					<div class="col-12">
+						<div class="card" style="width: 13rem;">
+							<img src="/Parcial_1/img/gaseosa-1.jpg" class="card-img-top img-col" alt="gaseosa-1">
+						</div>
+					</div>
+				</div>
+				<div class="row py-2">
+					<div class="col-12">
+						<div class="card" style="width: 13rem;">
+							<img src="/Parcial_1/img/gaseosa-2.jpg" class="card-img-top img-col" alt="gaseosa-2">
+						</div>
+					</div>
+				</div>
+				<div class="row py-2">
+					<div class="col-12">
+						<div class="card" style="width: 13rem;">
+							<img src="/Parcial_1/img/gaseosa-3.jpg" class="card-img-top img-col" alt="gaseosa-3">
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="second">
+			<a name="titulo">
+
+
+
+
+			</a>
+
+
+	</div>
+
+
+
+
+
+
+	<!-- Option 1: Bootstrap Bundle with Popper -->
+	<script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+	</script>
+</body>
+
+</html>
